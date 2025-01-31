15d4e24b8d00d39681d2112564a44c6bef0e7688
Stanislaw Makarchuk <49611600+StasMakarchuk@users.noreply.github.com>
2025-01-31 12:54:46 +0000

    Update palom_HE_DAPI.py
    added option to submit multiplane image

--- palom/palom_HE_DAPI.py
+++ palom/palom_HE_DAPI.py
@@ -4,13 +4,29 @@ import numpy as np
 import pandas as pd
 import fire
 import os
+import tifffile
+import dask.array as da
 
 
-
-def GetImages(HE_image_path, DAPI_image_path, level, thumbnail):
-    c1r = palom.reader.OmePyramidReader(DAPI_image_path)
+def GetImages(HE_image_path, DAPI_image_path, level, thumbnail, single_plane):
     c2r = palom.reader.OmePyramidReader(HE_image_path)
-    img1 = c1r.read_level_channels(0, 0)
+    c1r = palom.reader.OmePyramidReader(DAPI_image_path)
+    
+    if single_plane:
+        img1 = c1r.read_level_channels(0, 0)
+    else:
+        with tifffile.TiffFile(DAPI_image_path) as tif:
+            shape = tif.series[0].shape
+        if len(shape) > 3:
+            raise ValueError("Image has more than 3 dimensions: " + str(shape))
+        elif len(shape) == 2:
+            raise ValueError("Image has only 2 dimensions: " + str(shape) + '. If it is the case make sure to use flag single_plane = True')    
+        elif len(shape) == 3:
+            planes_da = [c1r.read_level_channels(0, i) for i in range(shape[0])]
+            img1 = da.stack(planes_da, axis=0).max(axis=0)
+        else:
+             raise ValueError("Image has weird dimensions: " + str(shape))
+        
     img1 = img1[::2**level, ::2**level]
     img2 = c2r.read_level_channels(0, 1) #using G channel for better contrast
     img2 = img2[::2**level, ::2**level]
@@ -18,9 +34,9 @@ def GetImages(HE_image_path, DAPI_image_path, level, thumbnail):
     img2_thumbnail = img2[::2**thumbnail, ::2**thumbnail]
     return img1, img2, img1_thumbnail, img2_thumbnail, c1r, c2r
 
-def RegisterOneImage(HE_image_path, DAPI_image_path, out_folder, name, level=0, thumbnail=5):
+def RegisterOneImage(HE_image_path, DAPI_image_path, out_folder, name, level=0, thumbnail=5, single_plane = True):
     print('reading images')
-    img1, img2, img1_thumbnail, img2_thumbnail, c1r, c2r = GetImages(HE_image_path, DAPI_image_path, level, thumbnail)
+    img1, img2, img1_thumbnail, img2_thumbnail, c1r, c2r = GetImages(HE_image_path, DAPI_image_path, level, thumbnail, single_plane)
     c21l = palom.align.Aligner(ref_img=img1, moving_img=img2, ref_thumbnail=img1_thumbnail, moving_thumbnail=img2_thumbnail,
                                ref_thumbnail_down_factor=2**thumbnail/2**level,
                                moving_thumbnail_down_factor=2**thumbnail/2**level)
@@ -36,21 +52,31 @@ def RegisterOneImage(HE_image_path, DAPI_image_path, out_folder, name, level=0, 
         mxs=c21l.block_affine_matrices_da)
     print('saving images')
     out_path = os.path.join(out_folder, name + '_reg.ome.tif')
-    palom.pyramid.write_pyramid(
-        mosaics=[
-            c1r.pyramid[level],
-            c2m
-        ],
-        output_path=out_path,
-        pixel_size=c1r.pixel_size*c1r.level_downsamples[level])
+    if single_plane:
+        palom.pyramid.write_pyramid(
+            mosaics=[
+                c1r.pyramid[level],
+                c2m
+            ],
+            output_path=out_path,
+            pixel_size=c1r.pixel_size*c1r.level_downsamples[level])
+    else:
+        palom.pyramid.write_pyramid(
+            mosaics=[
+                img1,
+                c2m
+            ],
+            output_path=out_path,
+            pixel_size=c1r.pixel_size*c1r.level_downsamples[level])
 
-def main(csv_table_path, out_folder, level = 0, thumbnail = 5):
+def main(csv_table_path, out_folder, single_plane = True, level = 0, thumbnail = 5):
     #csv file should have next columns: Name, HE_image_path, DAPI_image_path
     # at the moment I consider only one channel image for DAPI! (in future I want to consider N channels)
     table = pd.read_csv(csv_table_path)
     for i in range(table.shape[0]):
         print(table['Name'][i])
-        RegisterOneImage(table['HE_image_path'][i], table['DAPI_image_path'][i], out_folder, table['Name'][i], level, thumbnail)
+        if 'single_plane' in table.columns: single_plane = table['single_plane'][i]
+        RegisterOneImage(table['HE_image_path'][i], table['DAPI_image_path'][i], out_folder, table['Name'][i], level, thumbnail, single_plane)
 
 if __name__ == "__main__":
     fire.Fire(main)  
